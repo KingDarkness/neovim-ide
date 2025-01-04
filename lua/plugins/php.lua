660fc6d244a78c3d9e9551189edd4187326cb754
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2025-01-04 08:50:49 +0700

    :boom:

---
+++ lua/plugins/php.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        phpactor = {
+          filetypes = { "php", "blade", "php_only" },
+          init_options = {
+            ["language_server_completion.trim_leading_dollar"] = true,
+          },
+        },
+      },
+    },
+  },
+}
