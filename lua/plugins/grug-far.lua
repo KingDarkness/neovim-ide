eb7dda6050e287502ecb52395e0d517d74c454cf
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2025-03-20 14:38:27 +0700

    :boom:

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -5,7 +5,7 @@ local function search_on_directory(opts)
   opts.paths = utils.get_absolute_forder_path()
   if opts.select_word then
     opts.search = vim.fn.expand("<cword>")
-    require("grug-far").grug_far({ prefills = opts, transient = true })
+    require("grug-far").open({ prefills = opts, transient = true })
   else
     require("grug-far").with_visual_selection({ prefills = opts, transient = true })
   end
@@ -18,8 +18,8 @@ return {
     opts = { headerMaxWidth = 80 },
     -- stylua: ignore
     keys = {
-      { "<leader>F", function() require('grug-far').grug_far({transient = true}) end, desc = "Search & Replace in Project", },
-      { "<leader>fw", function() require('grug-far').grug_far({ prefills = { search = vim.fn.expand("<cword>") }, transient = true }) end, desc = "Search & Replace current word in Project" },
+      { "<leader>F", function() require('grug-far').open({transient = true}) end, desc = "Search & Replace in Project", },
+      { "<leader>fw", function() require('grug-far').open({ prefills = { search = vim.fn.expand("<cword>") }, transient = true }) end, desc = "Search & Replace current word in Project" },
       { "<leader>fD", function() search_on_directory() end, desc = "Search & Replace in current directory", mode = { "n", "v" } },
       { "<leader>fd", function() search_on_directory({select_word = true}) end, desc = "Search & Replace current word in current directory" },
     },
