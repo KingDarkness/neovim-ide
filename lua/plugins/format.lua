1c9320356fdbf69a5e0bc658583b4abd56445e06
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2026-01-23 13:55:28 +0700

    feat: add opencode

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -4,5 +4,11 @@ return {
     formatters_by_ft = {
       blade = { "blade_formatter" },
     },
+    formatters = {
+      ["php-cs-fixer"] = {
+        -- Thêm flag này để Mason/Conform không tạo file .cache nữa
+        prepend_args = { "--using-cache=no" },
+      },
+    },
   },
 }
