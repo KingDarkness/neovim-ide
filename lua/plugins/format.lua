40818c9a1fec60f25510a462558ec56b57d49442
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2026-07-30 14:50:45 +0700

    :boom:

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -3,9 +3,16 @@ return {
   opts = {
     formatters_by_ft = {
       blade = { "blade_formatter" },
+      php = { "pint", "php_cs_fixer", stop_after_first = true },
     },
     formatters = {
-      ["php-cs-fixer"] = {
+      pint = {
+        condition = function(self, ctx)
+          -- Chỉ dùng pint nếu tìm thấy vendor/bin/pint trong thư mục gốc của project
+          return vim.fs.find({ "vendor/bin/pint" }, { path = ctx.dirname, upward = true })[1] ~= nil
+        end,
+      },
+      php_cs_fixer = {
         -- Thêm flag này để Mason/Conform không tạo file .cache nữa
         prepend_args = { "--using-cache=no" },
       },
